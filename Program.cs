e4c4395e3b06d74a2c924114d3c167ac0b839828
karolrdg <karolinerodrigues12@outlook.com>
2025-09-23 18:24:13 -0300

    Subindo novo projeto para o repositório

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using ExemploPoo.Models;
+
+
+
+ContaCorrente conta = new ContaCorrente(12345, 1000);
+conta.ExibirSaldo();
+conta.Sacar(2000);
+conta.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa pessoa = new Pessoa()
+// {
+//     Nome = "Fulano",
+//     Idade = 30
+// };
+
+// pessoa.Apresentar();
